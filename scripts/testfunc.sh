e0d1e71fbf0fdb7eae5263deb7f19413f48c406d
Adrien Mahieux <adrien@mahieux.net>
2019-07-09 02:00:15 +0200

    [Add] testfunc allows to test a single function from a lib

---
+++ scripts/testfunc.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o noclobber
+
+export LC_ALL=C
+export PATH="/bin:/sbin:/usr/bin:/usr/sbin:$PATH"
+
+readonly MYSELF="$(readlink -f $0)"
+readonly MYPATH="${MYSELF%/*}"
+
+. $MYPATH/../ammlib
+
+
+[[ $# -lt 1 ]] && {
+	echo "Usage: $0 <function> [args]"
+	exit 0
+}
+
+typeset func="$1"; shift
+typeset libname="${func#amm}"
+libname="${libname,}"
+libname="${libname%%[A-Z]*}"
+
+ammLibLoad "$libname"
+
+$func "$@"

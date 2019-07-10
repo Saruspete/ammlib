a87af3d65653eb03c76d97ea23b628931d555bf5
Adrien Mahieux <adrien@mahieux.net>
2019-07-11 00:42:20 +0200

    [Add][scripts/_template] Add template for scripts

---
+++ scripts/_template.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o noclobber
+
+export LC_ALL=C
+export PATH="/bin:/sbin:/usr/bin:/usr/sbin:$PATH"
+
+readonly MYSELF="$(readlink -f $0)"
+readonly MYPATH="${MYSELF%/*}"
+
+. $MYPATH/../ammlib
+
+ammLibLoad "MODULE TO TEST"
+

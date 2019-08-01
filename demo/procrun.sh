066f35206a29059d08c66c9e530ee7a3c473e5aa
Adrien Mahieux <adrien@mahieux.net>
2019-08-02 01:13:04 +0200

    [Add][demo/procrun] Some demo for using process library

---
+++ demo/procrun.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o noclobber
+
+export LC_ALL=C
+export PATH="/bin:/sbin:/usr/bin:/usr/sbin:$PATH"
+export PS4=' (${BASH_SOURCE##*/}::${FUNCNAME[0]:-main}::$LINENO)  '
+
+readonly MYSELF="$(readlink -f $0)"
+readonly MYPATH="${MYSELF%/*}"
+
+. $MYPATH/../ammlib
+
+ammLibLoad process network
+
+ammProcessRun ammNetworkTraceroute 9.9.9.9

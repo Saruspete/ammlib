1b7e804f505e613a15487e11cfa88c75683d6bff
Adrien Mahieux <adrien@mahieux.net>
2019-12-08 00:07:21 +0100

    [Fix][demo/input.sh] fix validation naming

--- demo/input.sh
+++ demo/input.sh
@@ -14,18 +14,18 @@ readonly MYPATH="${MYSELF%/*}"
 ammLib::Require input
 
 ammInput::Register "toto"
-ammInput::Register "hostname" "Hostname of target machine" "localhost" "ammInputValidateHost"
-ammInput::Register "age" "Whats the age of the captain" "666" "ammInputValidateInt"
+ammInput::Register "hostname" "Hostname of target machine" "localhost" "ammInput::ValidateHost"
+ammInput::Register "age" "Whats the age of the captain" "666" "ammInput::ValidateInt"
 
 ammInput::Populate
 ammInput::Populate cmdline ""
 
-ammInput::Batch AutoTry
+#ammInput::Batch AutoTry
 #ammInput::Batch +DieOnErr
 
 echo toto = $(ammInput::Get "toto")
-echo titi = $(ammInput::Get "titi" "Any non empty value" "" "" "ammInputValidateNotEmpty")
+echo titi = $(ammInput::Get "titi" "Any non empty value" "" "" "ammInput::ValidateNotEmpty")
 echo host = $(ammInput::Get "hostname")
-echo lvm  = $(ammInput::Get "rd.lvm.lv" "LVM LV To activate" "" "" "ammInputValidateNotEmpty")
+echo lvm  = $(ammInput::Get "rd.lvm.lv" "LVM LV To activate" "" "" "ammInput::ValidateNotEmpty")
 
 ammInput::Blueprint

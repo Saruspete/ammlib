db99ca96135e4ab1bf6792a31fc060ed2f40fa36
Adrien Mahieux <adrien@mahieux.net>
2019-12-12 02:18:59 +0100

    [Enh][demo/storage.sh] Add new info from lib

--- demo/storage.sh
+++ demo/storage.sh
@@ -20,20 +20,22 @@ echo $blockdevs
 
 echo
 ammLog::Step "Fetching more details on each of these block"
-typeset fmt="%-15.15s %3.3s %3.3s %-10.10s %10.10s %-32.32s %-32.32s %s\n"
-printf "$fmt" "block name" "maj" "min" "type" "size" "serial" "firmware" "mountpoint"
+typeset fmt="%-15.15s %3.3s %3.3s %-10.10s %10.10s %-32.32s %-32.32s %-16.16s %s\n"
+printf "$fmt" "block name" "maj" "min" "type" "size" "model" "serial" "firmware" "mountpoint"
 
 typeset blockdev
 for blockdev in $blockdevs; do
-	typeset maj min typ block size sizeReadable serial firmware
+	typeset maj= min= typ= block= size= sizeReadable= model= serial= firmware= mnt=
 	read maj min < <(ammStorage::GetMajorMinor $blockdev)
 	size="$(ammStorage::GetSize $blockdev)"
 	sizeReadable="$(ammString::UnitConvert "$size" "B" "MB") MB"
 	typ="$(ammStorage::GetType $blockdev)"
+	model="$(ammStorage::GetModel $blockdev)"
 	serial="$(ammStorage::GetSerial $blockdev)"
 	firmware="$(ammStorage::GetFirmware $blockdev)"
+	mnt="$(ammStorage::GetMountpoint $blockdev)"
 
-	printf "$fmt" "$blockdev" "$maj" "$min" "$typ" "$sizeReadable" "$serial" "$firmware"
+	printf "$fmt" "$blockdev" "$maj" "$min" "$typ" "$sizeReadable" "$model" "$serial" "$firmware" "$mnt"
 done
 
 ammLog::Step "Real block devices status"

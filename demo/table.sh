1e4ef78b098012f68c17dfd65ce9ca1cb76d17ab
Adrien Mahieux <adrien.mahieux@gmail.com>
2020-01-28 02:21:01 +0100

    [Add][lib/table] Add simple table management

---
+++ demo/table.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o noclobber
+
+export LC_ALL=C
+export PATH="/bin:/sbin:/usr/bin:/usr/sbin:$PATH"
+
+readonly MYSELF="$(readlink -f $0)"
+readonly MYPATH="${MYSELF%/*}"
+
+. $MYPATH/../ammlib
+
+ammLib::Require "table"
+
+ammTable::Create "Demo table" "ID|size:4,overflow:hidden"  "Name|size:16,overflow:wrap" "Column 25%|size:25%" "Description (fill remains)|size:fill,overflow:wrap"
+
+ammTable::AddRow "0" "Title 1" "Useless" "This is a funny description"
+
+ammTable::Display
+
+typeset -i i=1
+while [[ $i -lt 20 ]]; do
+	ammTable::AddRow "$i" "Name $RANDOM" "Lorem ipsum" "Useless desc"
+	i+=1
+done
+
+ammTable::Display "direct"
+while [[ $i -lt 40 ]]; do
+	ammTable::AddRow "$i" "Name $RANDOM" "mouarf" "haha"
+	sleep .1
+	i+=1
+done

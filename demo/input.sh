e68632e89cd9adee859c77b56613ab2a3d9876ba
Adrien Mahieux <adrien.mahieux@gmail.com>
2020-09-12 02:51:38 +0200

    [Enh][lib/input] Make input work with optparse

--- demo/input.sh
+++ demo/input.sh
@@ -11,21 +11,17 @@ readonly MYPATH="${MYSELF%/*}"
 
 . $MYPATH/../ammlib
 
-ammLib::Require input
+ammLib::Require input optparse
 
 ammInput::Register "toto"
 ammInput::Register "hostname" "Hostname of target machine" "localhost" "ammInput::ValidateHost"
 ammInput::Register "age" "Whats the age of the captain" "666" "ammInput::ValidateInt"
 
-ammInput::Populate
-ammInput::Populate cmdline ""
-
-#ammInput::Batch AutoTry
-#ammInput::Batch +DieOnErr
+ammOptparse::Parse
 
 echo toto = $(ammInput::Get "toto")
-echo titi = $(ammInput::Get "titi" "Any non empty value" "" "" "ammInput::ValidateNotEmpty")
 echo host = $(ammInput::Get "hostname")
+echo titi = $(ammInput::Get "titi" "Any non empty value" "" "" "ammInput::ValidateNotEmpty")
 echo lvm  = $(ammInput::Get "rd.lvm.lv" "LVM LV To activate" "" "" "ammInput::ValidateNotEmpty")
 
-ammInput::Blueprint
+echo "Done. Try to call this script again with '--input-blueprint' to see its blueprint"

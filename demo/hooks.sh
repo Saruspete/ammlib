a69ce0104a22c9c13bfd897bcbaf0289442f0049
Adrien Mahieux <adrien.mahieux@gmail.com>
2022-06-20 02:13:48 +0200

    [Enh][ammlib] Simplify hooks and allow for recursive registration and triggering

--- demo/hooks.sh
+++ demo/hooks.sh
@@ -17,10 +17,37 @@ function testcb {
 }
 
 
+# Sample hook registration
 ammLib::HookRegister mycb testcb mycb
 ammLib::HookRegister libload testcb libload
-
 
 ammLib::Require string
 
 ammLib::HookTrigger mycb "Hello I've triggered mycb"
+
+
+
+#
+# Let's register a hook, when called will register another hook, and again.
+# Usage: Load a generic library, do early parsing to know what sub-library to load
+#        That sub-library may also want to load other elements (like optparse options)
+#
+function cbLink1 {
+	typeset myevent="$1"
+	ammLib::HookRegister "$myevent" cbLink2 "$myevent"
+	echo "$FUNCNAME registered cbLink2"
+}
+function cbLink2 {
+	typeset myevent="$1"
+	ammLib::HookRegister "$myevent" cbLink3 "$myevent"
+	echo "$FUNCNAME registered cbLink3"
+}
+function cbLink3 {
+	echo "cbLink3 has been called !"
+}
+
+# Hooks that registers on the same hook
+ammLib::HookRegister myEvent cbLink1 "myEvent"
+ammLib::HookTrigger  myEvent "Generated myEvent"
+
+
